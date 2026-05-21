de1da63038b8c34afa683c9a015ab597f80970f4
DM <dervismartinez20@gmail.com>
2026-05-21 00:39:04 -0400

    creacion de la ruta para autenticacion y usuarios : - instalacion de la libreria de encriptacion de contraseñas bcrypt - actualizacion de db,js con la tabla users - creacion de la ruta de autenticacion en index,js - conexion a frotn

--- utils/services/mailer.js
+++ utils/services/mailer.js
@@ -8,6 +8,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS 
+  },
+  tls: {
+    // Relaja la seguridad para servidores Plesk/cPanel con certificados internos o auto-firmados
+    rejectUnauthorized: false
   }
 });
 
